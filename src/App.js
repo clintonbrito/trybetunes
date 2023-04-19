663598000e9f37d36c79be9bda3465ec156b2c90
Clinton Brito <clintoncsb@gmail.com>
2023-04-19 13:46:19 -0300

    1o requisito ok v2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
+import Favorites from './pages/Favorites';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <h1>TrybeTuness</h1>
+        <main>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route exact path="/search" component={ Search } />
+            <Route exact path="/album/:id" component={ Album } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route exact path="/favorites" component={ Favorites } />
+            <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </main>
+      </div>
+    );
   }
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Header extends Component {
+  render() {
+    return (
+      <header data-testid="header-component" className="header">
+        Header
+      </header>
+    );
+  }
+}
+
+export default Header;
