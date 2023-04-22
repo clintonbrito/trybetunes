ef72734aacc8a61ce15300b35ad407dfad673ad8
Clinton Brito <clintoncsb@gmail.com>
2023-04-21 21:48:55 -0300

    4o requisito ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -24,7 +25,18 @@ export default class Header extends Component {
     const { name, isLoading } = this.state;
     return (
       <header data-testid="header-component" className="header">
-        { isLoading ? <Loading /> : <p data-testid="header-user-name">{name}</p> }
+        { isLoading ? <Loading /> : <div data-testid="header-user-name">{name}</div> }
+        <div>
+          <Link to="/search">
+            <div data-testid="link-to-search">Pesquisa</div>
+          </Link>
+          <Link to="/favorites">
+            <div data-testid="link-to-favorites">Favoritos</div>
+          </Link>
+          <Link to="/profile">
+            <div data-testid="link-to-profile">Perfil</div>
+          </Link>
+        </div>
       </header>
     );
   }
